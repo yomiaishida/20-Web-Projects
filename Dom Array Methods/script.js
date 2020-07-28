579a734dc0aa763b299a3cc3d6d4fc8096f6ad97
Abayomi Aishida <yomicorperate@gmail.com>
2020-07-28 23:59:20 +0100

    generating random users

--- Dom Array Methods/script.js
+++ Dom Array Methods/script.js
@@ -1,5 +1,5 @@
 const main = document.getElementById("main");
-const addUserBtn = document.getElementById("adduser");
+const addUserBtn = document.getElementById("add-user");
 const doubleBtn = document.getElementById("double");
 const showMillionairesBtn = document.getElementById("show-millionaires");
 const sortBtn = document.getElementById("sort");
@@ -7,11 +7,15 @@ const calculateWealthBtn = document.getElementById("calculate-wealth");
 
 let data = [];
 
+getRandomUser();
+getRandomUser();
+
 // Fetch random user and add money
 async function getRandomUser() {
   const res = await fetch("https://randomuser.me/api");
   const data = await res.json();
 
+  console.log(data);
   const user = data.results(0);
 
   const newUser = {
@@ -25,4 +29,19 @@ async function getRandomUser() {
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = "<h2><strong>Person</strong> Wealth</h2>";
+
+  providedData.forEach((item) => {
+    const element = document.createElement("div");
+    element.classList.add("person");
+    element.innerHTML = `<strong>${item.name}</strong> ${item.money}`;
+    main.appendChild(element);
+  });
 }
