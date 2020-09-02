21e8662fc67df2e60c8382e77357fcbf06f50ffb
Abayomi Aishida <yomicorperate@gmail.com>
2020-09-02 23:54:40 +0100

    Add new transaction to DOM

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -15,6 +15,35 @@ const dummyTransactions = [
 
 let transactions = dummyTransactions;
 
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === "" || amount.value.trim() === "") {
+    alert("Please add a text and amount");
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value,
+    };
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    text.value = "";
+    amount.value = "";
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 1000000);
+}
+
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
   // Get sign
@@ -64,3 +93,5 @@ function init() {
 }
 
 init();
+
+form.addEventListener("submit", addTransaction);
