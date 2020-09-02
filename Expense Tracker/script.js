8e0dc3afb449aa53e500bbf63ec54a52c6253ed1
Abayomi Aishida <yomicorperate@gmail.com>
2020-09-02 23:55:15 +0100

    Delete transactiokns from DOM

--- Expense Tracker/script.js
+++ Expense Tracker/script.js
@@ -57,7 +57,9 @@ function addTransactionDOM(transaction) {
   item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(
     transaction.amount
-  )}</span> <button class='delete-btn'>x</button>
+  )}</span> <button class='delete-btn' onClick="removeTransaction(${
+    transaction.id
+  })">x</button>
   `;
 
   list.appendChild(item);
@@ -82,6 +84,13 @@ function updateValues() {
   balance.innerText = `$${total}`;
   money_plus.innerText = `$${income}`;
   money_minus.innerText = `$${expense}`;
+}
+
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter((transaction) => transaction.id !== id);
+
+  init();
 }
 
 // Init app
