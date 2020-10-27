9cff3baba98c51fcabb5e3416edd79c978f1c6ea
Abayomi Aishida <yomicorperate@gmail.com>
2020-10-27 22:49:41 +0100

    Match words and update score

---
+++ Typing Game/script.js
@@ -0,0 +1,75 @@
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById(difficulty);
+
+// List of words
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "cuince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init tiime
+let time = 10;
+
+// Generate random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+// Add word to DOM
+function addWordToDom() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+// Update score
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+addWordToDom();
+
+// Event listener
+
+text.addEventListener("input", (e) => {
+  const insertedText = e.target.value;
+
+  if (insertedText === randomWord) {
+    addWordToDom();
+    updateScore();
+
+    // Clear
+    e.target.value = "";
+  }
+});
