de16fbcdb49c7afafcdb1860213bfad17096c763
Abayomi Aishida <yomicorperate@gmail.com>
2020-10-27 23:05:00 +0100

    Adding timer

--- Typing Game/script.js
+++ Typing Game/script.js
@@ -2,7 +2,7 @@ const word = document.getElementById("word");
 const text = document.getElementById("text");
 const scoreEl = document.getElementById("score");
 const timeEl = document.getElementById("time");
-const endgameEl = document.getElementById("end-game");
+const endgameEl = document.getElementById("end-game-container");
 const settingsBtn = document.getElementById("settings-btn");
 const settings = document.getElementById("settings");
 const settingsForm = document.getElementById("settings-form");
@@ -41,6 +41,12 @@ let score = 0;
 // Init tiime
 let time = 10;
 
+// Start counting down
+const timeInterval = setInterval(updateTime, 1000);
+
+// Focus on text on start
+text.focus();
+
 // Generate random word from array
 function getRandomWord() {
   return words[Math.floor(Math.random() * words.length)];
@@ -58,6 +64,29 @@ function updateScore() {
   scoreEl.innerHTML = score;
 }
 
+// Update time
+function updateTime() {
+  time--;
+  timeEl.innerHTML = time + "s";
+
+  if (time === 0) {
+    clearInterval(timeInterval);
+    // end game
+    gameOver();
+  }
+}
+
+// Game over show end screen
+function gameOver() {
+  endgameEl.innerHTML = `
+    <h1>Time ran out </h1>
+    <p>Your final score is ${score}</p>
+    <button onClick="location.reload()">Reload</button>
+  `;
+
+  endgameEl.style.display = "flex";
+}
+
 addWordToDom();
 
 // Event listener
@@ -71,5 +100,9 @@ text.addEventListener("input", (e) => {
 
     // Clear
     e.target.value = "";
+
+    time += 5;
+
+    updateTime();
   }
 });
