4b8aa2ca58d2e2f08962e194aa4cbf40be3a8d9a
Abayomi Aishida <yomicorperate@gmail.com>
2020-11-03 00:15:58 +0100

    Making speeches with web speech api

--- Text Reader/script.js
+++ Text Reader/script.js
@@ -70,10 +70,20 @@ function createBox(item) {
     <p class="info">${text}</p>
   `;
 
-  // @todo Speak event
+  box.addEventListener("click", () => {
+    setTextMessage(text);
+    speakText();
+
+    // Add active effect
+    box.classList.add("active");
+    setTimeout(() => box.classList.remove("active"), 800);
+  });
 
   main.appendChild(box);
 }
+
+// Init speech synth
+const message = new SpeechSynthesisUtterance();
 
 // Store voices
 let voices = [];
@@ -89,6 +99,16 @@ function getVoices() {
 
     voicesSelect.appendChild(option);
   });
+}
+
+// Set text
+function setTextMessage(text) {
+  message.text = text;
+}
+
+// Speak text
+function speakText() {
+  speechSynthesis.speak(message);
 }
 
 // Voices changed
