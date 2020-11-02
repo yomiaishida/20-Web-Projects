00a847822373c81763be0a9caef98e4f03cd314e
Abayomi Aishida <yomicorperate@gmail.com>
2020-11-03 00:05:41 +0100

    displaying speech voices in select

--- Text Reader/script.js
+++ Text Reader/script.js
@@ -1,5 +1,5 @@
 const main = document.querySelector("main");
-const voiceSelect = document.getElementById("voice");
+const voicesSelect = document.getElementById("voices");
 const textarea = document.getElementById("text");
 const readBtn = document.getElementById("reac");
 const toggleBtn = document.getElementById("toggle");
@@ -58,7 +58,7 @@ const data = [
 
 data.forEach(createBox);
 
-// Create speecp boxes
+// Create speech boxes
 function createBox(item) {
   const box = document.createElement("div");
 
@@ -74,3 +74,34 @@ function createBox(item) {
 
   main.appendChild(box);
 }
+
+// Store voices
+let voices = [];
+
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+
+  voices.forEach((voice) => {
+    const option = document.createElement("option");
+
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+
+    voicesSelect.appendChild(option);
+  });
+}
+
+// Voices changed
+speechSynthesis.addEventListener("voiceschanged", getVoices);
+
+// Toggle text box
+toggleBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.toggle("show")
+);
+
+// Close button
+closeBtn.addEventListener("click", () =>
+  document.getElementById("text-box").classList.remove("show")
+);
+
+getVoices();
