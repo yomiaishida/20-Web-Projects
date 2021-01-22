859669a20175e8908f988db7a11654619234e3e1
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-22 14:50:33 +0100

    Increasing the score and resetting game when player loses

--- Breakout/script.js
+++ Breakout/script.js
@@ -137,10 +137,32 @@ function moveBall() {
         ) {
           ball.dy *= -1;
           brick.visible = false;
+
+          increaseScore();
         }
       }
     });
   });
+
+  // Hit bottom wall - Lose
+  if (ball.y + ball.size > canvas.height) {
+    showAllBricks();
+    score = 0;
+  }
+}
+
+// Increase score
+function increaseScore() {
+  score++;
+
+  if (score % (brickRowCount * brickRowCount) === 0) {
+    showAllBricks();
+  }
+}
+
+// Make all bricks appear
+function showAllBricks() {
+  bricks.forEach((column) => column.forEach((brick) => (brick.visible = true)));
 }
 
 // Draw everything
