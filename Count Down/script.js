6ab88573c43858559748a25c5f24bf832f8d03dd
Abayomi Aishida <yomicorperate@gmail.com>
2021-01-29 23:52:56 +0100

    Added dynamic year on the background and loading a spinner

--- Count Down/script.js
+++ Count Down/script.js
@@ -3,15 +3,22 @@ const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
 const countdown = document.getElementById("countdown");
+const year = document.getElementById("year");
+const loading = document.getElementById("loading");
 
 const currentYear = new Date().getFullYear();
 
 const newYearTime = new Date(`January 01 ${currentYear + 1} 00:00:00`);
 
+// Set background year
+year.innerText = currentYear + 1;
+
+// Update countdown time
 function updateCountdown() {
   const currentTime = new Date();
   const diff = newYearTime - currentTime;
 
+  // Add values to DOM
   const d = Math.floor(diff / 1000 / 60 / 60 / 24);
   const h = Math.floor(diff / 1000 / 60 / 60) % 24;
   const m = Math.floor(diff / 1000 / 60) % 60;
@@ -25,4 +32,11 @@ function updateCountdown() {
   console.log(s);
 }
 
+// Show spinner before countdown
+setTimeout(() => {
+  loading.remove();
+  countdown.style.display = "flex";
+}, 1000);
+
+// Run every second
 setInterval(updateCountdown, 1000);
