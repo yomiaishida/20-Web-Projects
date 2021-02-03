f86e1cc0e41acb407038c211809f09a43ef61fc9
Abayomi Aishida <yomicorperate@gmail.com>
2021-02-03 21:41:53 +0100

    Checking order

--- Drag&DropApi/script.js
+++ Drag&DropApi/script.js
@@ -76,12 +76,27 @@ function dragDrop() {
   this.classList.remove("over");
 }
 
+// Swap list items that are drag and drop
 function swapItems(fromIndex, toIndex) {
   const itemOne = listItems[fromIndex].querySelector(".draggable");
   const itemTwo = listItems[toIndex].querySelector(".draggable");
 
   listItems[fromIndex].appendChild(itemTwo);
   listItems[toIndex].appendChild(itemOne);
+}
+
+// Check the order of list items
+function checkOrder() {
+  listItems.forEach((listItem, index) => {
+    const personName = listItem.querySelector(".draggable").innerText.trim();
+
+    if (personName !== richestPeople[index]) {
+      listItem.classList.add("wrong");
+    } else {
+      listItem.classList.remove("wrong");
+      listItem.classList.add("right");
+    }
+  });
 }
 
 function addEventListeners() {
@@ -99,3 +114,5 @@ function addEventListeners() {
     item.addEventListener("dragleave", dragLeave);
   });
 }
+
+check.addEventListener("click", checkOrder);
